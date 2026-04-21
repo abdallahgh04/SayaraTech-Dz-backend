cf20607ea2a459d2bc20cb81a2d1bcb496835f36
abdallahgh04 <168266476+abdallahgh04@users.noreply.github.com>
2026-04-21 18:24:17 +0100

    06

--- config/middlewares.js
+++ config/middlewares.js
@@ -18,20 +18,12 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       origin: [
-        // Production
         'https://www.sayaratechdz.store',
         'https://sayaratechdz.store',
-        // Local dev
         'http://localhost:3000',
         'http://localhost:5173',
         'http://localhost:5174',
         'http://localhost:5175',
-        'http://10.33.111.16:5173',
-        'http://10.33.111.16:5174',
-        // OAuth providers
-        'https://accounts.google.com',
-        'https://www.facebook.com',
-        'https://graph.facebook.com',
       ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
       headers: ['*'],
