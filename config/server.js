784b3161abf5753017c4c75a4032ec8ba2d47b81
abdallahgh04 <168266476+abdallahgh04@users.noreply.github.com>
2026-04-22 02:24:00 +0200

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://tranquil-transformation-production-5166.up.railway.app'),
   app: {
     keys: env.array('APP_KEYS'),
   },
